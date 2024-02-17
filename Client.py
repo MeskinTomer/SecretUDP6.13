91dad491f78f5e262a5585eede665a9c443f5f30
Tomer <meskintomer@gmail.com>
2024-02-17 21:19:10 +0200

    Starting commit and base code

---
+++ Client.py
@@ -0,0 +1,14 @@
+"""
+Author: Tomer Meskin
+Date: 16/02/24
+Client of secret communication above UDP
+"""
+
+# Imports
+from scapy.all import *
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
