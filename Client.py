77798f0ba424a61e5b413d88679a967e56ce5bc2
Tomer <meskintomer@gmail.com>
2024-02-17 22:30:41 +0200

    Code finished

--- Client.py
+++ Client.py
@@ -6,6 +6,9 @@ Client of secret communication above UDP
 
 # Imports
 from scapy.all import *
+from scapy.layers.inet import IP, UDP
+from scapy.sendrecv import send
+
 
 def handle_message(message):
     """
@@ -13,12 +16,19 @@ def handle_message(message):
     :param message: The message from the client
     :return None
     """
+    server_ip = input("Enter the other's IP address: ")
+    for char in message:
+        ascii_val = ord(char)
+        packet = IP(dst=server_ip) / UDP(dport=ascii_val)
+        send(packet)
+
 
 def main():
     message = input("Enter your message here: ")
     print("Handling message")
     handle_message(message)
     print("Message sent")
+
 
 if __name__ == '__main__':
     main()
--- Server.py
+++ Server.py
@@ -6,6 +6,22 @@ Server of secret communication above UDP
 
 # Imports
 from scapy.all import *
+from scapy.layers.inet import UDP, IP
+
+
+def is_empty(packet):
+    """
+    Determines if the packet's payload is empty
+    :param packet: the UDP packet to be checked
+    :return: True - if packet is empty
+             False - if packet has data
+    """
+    empty = False
+    payload = packet[UDP].payload
+    if isinstance(payload, Padding) and payload.load == b'\x00' * len(payload.load):
+        empty = True
+    return empty
+
 
 def sniff_packets(packet):
     """
@@ -13,10 +29,18 @@ def sniff_packets(packet):
     :param packet: The intended UDP packet to decode
     :return: None
     """
+    if packet.haslayer(UDP) and is_empty(packet):
+        port = packet.dport
+        char_ascii = chr(port)
+        print(char_ascii, end="")
+
 
 def main():
-    sniff(prn=sniff_packets)
+    sniff(prn=sniff_packets, filter='udp')
 
 
 if __name__ == '__main__':
+    # Asserts
+    sample_packet = IP(dst="0.0.0.0") / UDP(dport=53421) / Raw(b"Hello World!")
+    assert not is_empty(sample_packet)
     main()
